385a7f6a30795a00b3b0c3f541a266770ae6a4d4
Minoas Chatzidakis <minoshatzidakis@gmail.com>
2026-02-14 20:36:22 +0200

    fixed git header errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-<<<<<<< HEAD
   before_action :set_active_chats
-=======
->>>>>>> ed40c09b3128879fedbfd2efadfb036c98c93b69
-  
+
   protect_from_forgery with: :exception, prepend: true
   allow_browser versions: :modern
 
   protected
-<<<<<<< HEAD
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -20,17 +16,10 @@ class ApplicationController < ActionController::Base
 
   def set_active_chats
     if user_signed_in?
-      # Χρησιμοποιούμε Arel.sql() για να πούμε στο Rails ότι το string είναι έμπιστο
       @active_chatrooms = Chatroom.where("chatrooms.name LIKE ?", "%#{current_user.id}%")
                                   .left_joins(:messages)
                                   .group("chatrooms.id")
                                   .order(Arel.sql("MAX(messages.created_at) DESC NULLS LAST"))
     end
-=======
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
->>>>>>> ed40c09b3128879fedbfd2efadfb036c98c93b69
   end
 end
--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 class ChatroomsController < ApplicationController
-=======
-
-class ChatroomsController < ApplicationController
-  def create
-    @other_user = User.find(params[:user_id])
-    name = [current_user.id, @other_user.id].sort.join("-")
-    @chatroom = Chatroom.find_or_create_by(name: name)
-    
-    redirect_to chatroom_path(@chatroom)
-  end
-
->>>>>>> ed40c09b3128879fedbfd2efadfb036c98c93b69
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
